56703be5df1a1151d45e3e3a67175ba78769003c
Roman Rudnytskiy <romarudnytskiy@gmail.com>
2020-02-25 11:57:45 +0200

    Initial commit

---
+++ Lab1/app/src/main/java/com/android/rrudnytskii/laboratorywork1/MainDisplay.java
@@ -0,0 +1,13 @@
+package com.android.rrudnytskii.laboratorywork1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_display);
+    }
+}
